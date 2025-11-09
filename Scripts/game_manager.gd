3f1449b674e84a5669bffa2666c1cb167f5c9553
Josh <joshy09090@yahoo.com>
2025-11-09 11:25:35 -0700

    Setup demo for 2d sprites
    Added:
    Spritesheets
    Player Controller handling of basic (1-3 frame) 2d sprite animation
    Modified player variables to be visible in editor for debugging
    Game Controller handling to load new scene
    Demo scene
    Tilemap demo
    2d collider setup for some grass textures in spritesheet as demo
    Background images tiled to show tilemaps on separate layers with various sizes.

---
+++ Scripts/game_manager.gd
@@ -0,0 +1,23 @@
+extends Node
+
+# Script used for managing game states
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+#	pass
+
+
+func load_level(level_path: String):
+	get_tree().change_scene_to_file(level_path)
+	print("Loading lavel: level_path")
+
+func _unhandled_input(event):
+	if event.is_action_pressed("load_next_level"):
+		load_level("res://Levels/Level01_Tiles_demo/level_1_tileset_demo.tscn")
+	
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,25 +1,44 @@
 extends CharacterBody2D
 
+#script for controlling player character sprite
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+#Get a reference to the animated sprite node attached to the Player root node
+@onready var player_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+#Changed player variables here to show them in editor for easier gameplay modifications.
+@export var SPEED = 300.0
+@export var JUMP_VELOCITY = -650.0
 
 
+#For physics processing I changed all inputs away from UI input events
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	#Replaced ui_left and ui_right with player controller actions not associated with the UI
+	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
+		player_sprite.flip_h = velocity.x < 0
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	#Pick sprite animation based on velocity
+	if is_on_floor() && velocity.x != 0:
+		player_sprite.play("walk")
+	elif velocity.y > 0:
+		player_sprite.play("falling")
+	elif velocity.y < 0:
+		player_sprite.play("jump")
+	else:
+		player_sprite.play("idle")
+	
+	
 
 	move_and_slide()
